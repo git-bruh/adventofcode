491ef1b96a23242bae80cde3aca74738eb72781c
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-12-02 18:32:37 +0530

    part 2

---
+++ 2_part2.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+import enum
+import sys
+
+class OTYPE(enum.Enum):
+	FORWARD = 0
+	DOWN = 1
+	UP = 2
+
+@dataclass
+class OP:
+	op: OTYPE
+	value: int
+
+str_to_op = {
+	"forward": OTYPE.FORWARD,
+	"down": OTYPE.DOWN,
+	"up": OTYPE.UP,
+}
+
+def get_op(line):
+	line = line.split()
+	return OP(str_to_op[line[0]], int(line[1]))
+
+ops = [get_op(line) for line in sys.stdin.read().split("\n") if line != ""]
+
+aim = 0
+horizontal = 0
+depth = 0
+
+for op in ops:
+	print(op)
+
+	match op.op:
+		case OTYPE.FORWARD:
+			horizontal += op.value
+			depth += (aim * op.value)
+		case OTYPE.DOWN:
+			aim += op.value
+		case OTYPE.UP:
+			aim -= op.value
+
+print(f"Horizontal: {horizontal}, Depth: {depth}, Result: {horizontal * depth}")
