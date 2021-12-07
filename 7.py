aabc6f9e09580951c369e6e5f323b5f429b201d1
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-12-07 12:48:47 +0530

    use formula instead of loop for 7

--- 7.py
+++ 7.py
@@ -5,19 +5,15 @@ positions = list(map(int, sys.stdin.read().split(",")))
 min_fuel = -1
 
 max_pos = max(positions)
-extra = defaultdict(int)  # Remove this for part 1
 
 for dest_pos in range(max_pos + 1):
     fuel = 0
 
     for position in positions:
         dist = abs(position - dest_pos)
+        dist += int((dist * (dist - 1) / 2))  # Sum, remove for part 1
 
-        if dist not in extra:
-            for i in range(dist):
-                extra[dist] += i
-
-        fuel += dist + extra[dist]
+        fuel += dist
 
     if min_fuel == -1 or fuel < min_fuel:
         min_fuel = fuel
