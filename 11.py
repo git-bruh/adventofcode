975df9b130df079faf13356b0c14d727c79eee8c
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-12-11 15:08:51 +0530

    11

---
+++ 11.py
@@ -0,0 +1,61 @@
+import sys
+
+matrix = []
+
+for line in sys.stdin.read().split("\n"):
+    if line:
+        matrix.append(list(map(int, line)))
+
+
+def flash_adjacent(row, col, buf, flashed):
+    if buf[row][col] <= 9 or (row, col) in flashed:
+        return
+
+    # An octopus can only flash at most once per step
+    flashed[(row, col)] = True
+
+    for row_get, col_get in [
+        [row, col - 1],
+        [row, col + 1],
+        [row - 1, col],
+        [row + 1, col],
+        # Diagonals
+        [row - 1, col - 1],
+        [row - 1, col + 1],
+        [row + 1, col - 1],
+        [row + 1, col + 1],
+    ]:
+        if row_get < 0 or col_get < 0:
+            continue
+
+        try:
+            buf[row_get][col_get] += 1
+            flash_adjacent(row_get, col_get, buf, flashed)
+        except IndexError:
+            pass
+
+
+num_flashed = 0
+
+for _ in range(100):
+    flashed = {}
+
+    for row_index, row in enumerate(matrix):
+        for col_index, col in enumerate(row):
+            matrix[row_index][col_index] += 1
+
+    for row_index, row in enumerate(matrix):
+        for col_index, col in enumerate(row):
+            flash_adjacent(row_index, col_index, matrix, flashed)
+
+    # Must reset after flashing
+    for flash in flashed:
+        row, col = flash
+        matrix[row][col] = 0
+
+    num_flashed += len(flashed)
+
+for line in matrix:
+    print(*line, sep="")
+
+print(num_flashed)
